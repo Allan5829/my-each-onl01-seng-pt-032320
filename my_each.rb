c25835294b590ee28141f22ff0f0099242109cda
Allan Farca <allan.farca@gmail.com>
2020-03-31 04:21:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
+array = []
+
 def my_each (00, 01, 10, 11, 100, 101, 110, 111)
-  # code here
+if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
